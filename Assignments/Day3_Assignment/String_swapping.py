4d197755ab0f04f76f385160ec6650bb00f441aa
ProgrammerVrushabh <vrushabhnipane723@gmail.com>
2026-07-15 11:32:59 +0530

    Add remaining Day 3 assignment file

---
+++ Assignments/Day3_Assignment/String_swapping.py
@@ -0,0 +1,13 @@
+#lower to upper and vice versa
+
+'''s1=input("Enter a string: ")
+s2=""
+for i in s1:
+    if i.islower():
+        s2+=i.upper()
+    elif i.isupper():
+        s2+=i.lower()
+    else:
+        s2+=i
+print("The swapped string is:", s2)'''
+
